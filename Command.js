04086be8d055f3ea290022f91afb2cd02fe02ecf
NIAZI-MD <niazimdofficialniazimd@gmail.com>
2025-10-31 07:48:40 +0500

    Create Command.js

---
+++ Command.js
@@ -0,0 +1,14 @@
+// command.js
+module.exports = {
+  MAIN_HEADER: `*╭┄┄✪ ˚₊· ͟͟͞͞➳❥ N I A Z I _ M D ⚚ ✪┄⊷*`,
+  MAIN_FOOTER: `*╰┈┈┈┈┈┈⊷*`,
+  SAMPLE_MENU: `
+*┃❂ Owner* : NIAZI_MD ( +92 344 8166105 )
+*┃❂ Prefix* : .
+*┃❂ Type* : Nodejs • Baileys MD
+*┃❂ Mode* : public
+
+Main Commands (samples):
+.ai .ask .menu .pair .ping .repo .download .sticker .yt .tiktok .setprefix
+`
+};
