e3ebf3d5e0d299e4a6c0207e22ee73079a538636
NIAZI-MD <niazimdofficialniazimd@gmail.com>
2025-10-31 11:53:47 +0500

    Delete Config.js

--- Config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-module.exports = {
-  OWNER_NUMBER: "923448166105",
-  BOT_NAME: "NIAZI-MD",
-  CHANNEL_LINK: "https://whatsapp.com/channel/0029VbBKWrA2v1Iu4KVE3A1H",
-  GROUP_LINK: "https://chat.whatsapp.com/KJ6qs3H2xC6AQPYRTaNBNm?mode=wwt",
-  MENU_IMAGE: "https://i.ibb.co/gbbGmx3G/shaban-md.jpg",
-  PREFIX: "."
-};
